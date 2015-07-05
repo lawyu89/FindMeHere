7eaa360cbe91183d0ac50a82ebbb1c801db6ce55
Lawrence Yu <lawrenceyu1@gmail.com>
2015-07-04 22:27:07 -0700

    Set initial configuration

--- server.js
+++ server.js
@@ -1,13 +1,54 @@
 var express = require('express'),
+
     passport = require('passport'),
-    FacebookStrategu = require('passport-facebook').Strategy,
-    GoogleStrategy - require('passport-google').Strategy,
+    FacebookStrategy = require('passport-facebook').Strategy,
+    GoogleStrategy = require('passport-google').Strategy,
     config = require('./oauth.js'),
     mongoose = require('mongoose');
 
 var app = express();
-app.configure(function(){
-  app.set('port', config.app.port)
+
+//setup MongoDB database for users
+mongoose.connect('mongodb://localhost/FindMeHere')
+
+var User = mongoose.model('User', {
+  oauthID: Number,
+  name: String
 })
 
-app.listen(8080)
+//serial and deserialize users
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+//app configuration
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(__dirname + '/public'));
+
+//passport oauth configuration
+passport.use(new FacebookStrategy({
+  clientID: config.facebook.clientID,
+  clientSecret: config.facebook.clientSecret,
+  callbackURL: config.facebook.callbackURL
+  }, function(accessToken, refreshToken, profile, done) {
+    process.nextTick(function(){
+      return done(null, profile);
+    });
+  }
+));
+
+//setup routes
+
+
+
+
+
+
+app.listen(8080);
+console.log("Listening on port 8080");
