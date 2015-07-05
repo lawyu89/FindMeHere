d93ba6d8cf3b11db86686b739feefd027a9ed519
Lawrence Yu <lawrenceyu1@gmail.com>
2015-07-04 18:33:12 -0700

    Update .gitignore file

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
-var express = require('express');
+var express = require('express'),
+    passport = require('passport'),
+    FacebookStrategu = require('passport-facebook').Strategy,
+    GoogleStrategy - require('passport-google').Strategy,
+    config = require('./oauth.js'),
+    mongoose = require('mongoose');
+
 var app = express();
+app.configure(function(){
+  app.set('port', config.app.port)
+})
+
+app.listen(8080)
